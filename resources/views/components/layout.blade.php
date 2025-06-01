88f5798ecc6988915473cf54bcc9f1aeae957b1b
waqqir humaid <waqqirhumaid1@gmail.com>
2025-06-01 23:08:05 +0700

    index layanan

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,14 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
     <title>Document</title>
 </head>
-<body class="bg-gray-200" >
+
+<body class="bg-gray-200">
     {{ $slot }}
+
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
 </body>
+
 </html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,19 +1,67 @@
 <x-layout>
     <x-nav-bar></x-nav-bar>
     <div class="relative bg-gray-700 h-90">
-  <!-- Hiasan kotak di belakang -->
-  <div class="absolute inset-0 -z-10">
-    <div class="w-64 h-64 bg-blue-100 rounded-xl absolute top-10 left-10"></div>
-    <div class="w-40 h-40 bg-pink-100 rounded-xl absolute bottom-10 right-10"></div>
-    <!-- Tambah dekorasi lain di sini kalau mau -->
-  </div>
-</div>
-<div class="bg-gray-400 bottom-50 relative xl:mx-20 sm:mx-3 md:mx-10 mx-2 rounded-2xl
-    h-100
-    {{-- buat matok aja itumahh --}}
-">
-    
-</div>
+        {{-- hiasan kotak d belakang --}}
+        <div class="absolute inset-0 -z-10">
+            <div class="w-64 h-64 bg-blue-100 rounded-xl absolute top-10 left-10"></div>
+            <div class="w-40 h-40 bg-pink-100 rounded-xl absolute bottom-10 right-10"></div>
 
+        </div>
+    </div>
+    <div class="bg-gray-400 bottom-50 relative xl:mx-20 sm:mx-3 md:mx-10 mx-2 rounded-2xl shadow-2xl
+{{-- buat matok aja itumahh --}}
+h-100
+">
+        <h1>Hello world</h1>
+    </div>
+
+    {{-- Layanan --}}
+    <div class="mx-10 my-30">
+        {{-- judul layanan --}}
+        <h1 class="text-center font-bold xl:text-5xl md:text-4xl text-3xl  hover:text-gray-500" data-aos="fade-down">
+            Layanan-Layanan Kami
+        </h1>
+
+        {{-- Container Card --}}
+        <div class="flex flex-wrap justify-center items-stretch gap-5 m-5">
+            
+            {{-- Card 1 --}}
+            <a href="#" class="w-full sm:w-1/2 lg:w-1/3 block p-6 bg-white border border-gray-200 rounded-lg shadow-sm
+              hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700
+              opacity-0 animate-fade-up card-delay-1" data-aos="fade-down" data-aos-easing="linear"
+                data-aos-duration="840">
+
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology
+                    acquisitions 2021</h5>
+                <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology
+                    acquisitions of 2021 so far, in reverse chronological order.</p>
+            </a>
+
+            {{-- Card 2 --}}
+            <a href="#" class="w-full sm:w-1/2 lg:w-1/3 block p-6 bg-white border border-gray-200 rounded-lg shadow-sm
+              hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700
+              opacity-0 animate-fade-up card-delay-2" data-aos="fade-down" data-aos-easing="linear"
+                data-aos-duration="840">
+
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology
+                    acquisitions 2021</h5>
+                <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology
+                    acquisitions of 2021 so far, in reverse chronological order.</p>
+            </a>
+
+            {{-- Card 3 --}}
+            <a href="#" class="w-full sm:w-1/2 lg:w-1/3 block p-6 bg-white border border-gray-200 rounded-lg shadow-sm
+              hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700
+              opacity-0 animate-fade-up card-delay-3" data-aos="fade-down" data-aos-easing="linear"
+                data-aos-duration="840">
+
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology
+                    acquisitions 2021</h5>
+                <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology
+                    acquisitions of 2021 so far, in reverse chronological order.</p>
+            </a>
+        </div>
+
+    </div>
 
 </x-layout>
